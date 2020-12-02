fb0263c9c42c0be28946964600d6cdcf4cdac87a
Jase Lindgren <jase@multiamory.com>
2020-12-01 20:16:58 -0800

    2020 day 1 answers. Not totally happy with the solution. It's a tiny bit optimized but not by much.

--- 2020/01/part_1.py
+++ 2020/01/part_1.py
@@ -11,3 +11,19 @@ input_string = get_input.get_input_for_day(__file__)
 # __________________ End of setup ____________________
 
 
+def main():
+    input_values = [int(line) for line in input_string.splitlines()]
+    matching_values = find_matching_values(input_values, 2020)
+    print(f"The two values are {matching_values[0]} and {matching_values[1]}")
+    print(f"The product is {matching_values[0] * matching_values[1]}")
+
+
+def find_matching_values(values, target):
+    for i, value_a in enumerate(values):
+        for value_b in values[i + 1 :]:
+            if value_a + value_b == target:
+                return value_a, value_b
+
+
+if __name__ == "__main__":
+    main()
--- 2020/01/part_2.py
+++ 2020/01/part_2.py
@@ -11,3 +11,24 @@ input_string = get_input.get_input_for_day(__file__)
 # __________________ End of setup ____________________
 
 
+def main():
+    input_values = [int(line) for line in input_string.splitlines()]
+    matching_values = find_matching_values(input_values, 2020)
+    print(
+        f"The three values are {matching_values[0]}, {matching_values[1]} and {matching_values[2]}"
+    )
+    print(
+        f"The product is {matching_values[0] * matching_values[1] * matching_values[2]}"
+    )
+
+
+def find_matching_values(values, target):
+    for a, value_a in enumerate(values):
+        for b, value_b in enumerate(values[a + 1 :]):
+            for value_c in values[b + a + 1 :]:
+                if value_a + value_b + value_c == target:
+                    return value_a, value_b, value_c
+
+
+if __name__ == "__main__":
+    main()
