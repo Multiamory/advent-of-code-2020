c9ab0021347a7ec1c85fb2f05988a022c863a898
Jase Lindgren <jase@multiamory.com>
2020-12-02 00:08:48 -0800

    Day 2 answers. Finally got to play around with namedtuple a little. Some of the lines are a little long and wordy. Could probably be split up a little better to be more readable.

--- 2020/02/part_1.py
+++ 2020/02/part_1.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from collections import namedtuple
 
 # ___________ Begin Setup and Get Input _____________
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
@@ -10,4 +11,46 @@ import get_input
 input_string = get_input.get_input_for_day(__file__)
 # __________________ End of setup ____________________
 
+PasswordPair = namedtuple("PasswordPair", ["policy", "password"])
 
+
+class Policy:
+    def __init__(self, policy_string):
+        policy_parts = policy_string.split(" ")
+        self.char = policy_parts[1]
+        self.min = int(policy_parts[0].split("-")[0])
+        self.max = int(policy_parts[0].split("-")[1])
+
+    def test_password(self, password):
+        char_count = password.count(self.char)
+        if char_count < self.min:
+            return False
+        if char_count > self.max:
+            return False
+        return True
+
+    def __repr__(self):
+        return f"{self.char}: {self.min}-{self.max}"
+
+
+def main():
+    input_tuples = tuple(
+        [
+            PasswordPair(Policy(line.split(":")[0]), line.split(":")[1])
+            for line in input_string.splitlines()
+        ]
+    )
+    print(f"Total valid password: {count_correct(input_tuples)}")
+
+
+def count_correct(input_tuples):
+    count = 0
+    for input_tuple in input_tuples:
+        if input_tuple.policy.test_password(input_tuple.password):
+            count += 1
+
+    return count
+
+
+if __name__ == "__main__":
+    main()
--- 2020/02/part_2.py
+++ 2020/02/part_2.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from collections import namedtuple
 
 # ___________ Begin Setup and Get Input _____________
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
@@ -10,4 +11,45 @@ import get_input
 input_string = get_input.get_input_for_day(__file__)
 # __________________ End of setup ____________________
 
+PasswordPair = namedtuple("PasswordPair", ["policy", "password"])
 
+
+class Policy:
+    def __init__(self, policy_string):
+        policy_parts = policy_string.split(" ")
+        self.char = policy_parts[1]
+        self.index_a = int(policy_parts[0].split("-")[0]) - 1
+        self.index_b = int(policy_parts[0].split("-")[1]) - 1
+
+    def test_password(self, password):
+        password = password.strip()
+        is_char_a = password[self.index_a] == self.char
+        is_char_b = password[self.index_b] == self.char
+
+        return is_char_a != is_char_b
+
+    def __repr__(self):
+        return f"{self.char}: {self.min}-{self.max}"
+
+
+def main():
+    input_tuples = tuple(
+        [
+            PasswordPair(Policy(line.split(":")[0]), line.split(":")[1])
+            for line in input_string.splitlines()
+        ]
+    )
+    print(f"Total valid password: {count_correct(input_tuples)}")
+
+
+def count_correct(input_tuples):
+    count = 0
+    for input_tuple in input_tuples:
+        if input_tuple.policy.test_password(input_tuple.password):
+            count += 1
+
+    return count
+
+
+if __name__ == "__main__":
+    main()
