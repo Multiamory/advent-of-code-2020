e7b1ec5ab6572c6ad0ba38c1e5c4497e45e69b50
Jase Lindgren <jase@multiamory.com>
2020-12-11 22:35:39 -0800

    day 9! Not so bad

--- 2020/09/part_1.py
+++ 2020/09/part_1.py
@@ -11,3 +11,45 @@ input_string = get_input.get_input_for_day(__file__)
 # __________________ End of setup ____________________
 
 
+def main(input_string, preamble_length):
+    input_items = tuple(int(n) for n in input_string.splitlines())
+
+    for i, number in enumerate(input_items[preamble_length:]):
+        if not check_if_valid(input_items[i : i + preamble_length], number):
+            return number
+
+    print("We didn't find it.")
+
+
+def check_if_valid(preamble, number):
+    for i, val_a in enumerate(preamble):
+        for val_b in preamble[i + 1 :]:
+            if val_a + val_b == number:
+                return True
+    return False
+
+
+if __name__ == "__main__":
+    test_input = """35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576"""
+    # res = main(test_input, 5)
+    res = main(input_string, 25)
+    print(f"The result is {res}")
--- 2020/09/part_2.py
+++ 2020/09/part_2.py
@@ -11,3 +11,43 @@ input_string = get_input.get_input_for_day(__file__)
 # __________________ End of setup ____________________
 
 
+def main(input_string, target):
+    input_items = tuple(int(n) for n in input_string.splitlines())
+    sequence = find_sequence_that_sums(input_items, 2, target)
+    return min(sequence) + max(sequence)
+
+
+def find_sequence_that_sums(input_items, num_items, target):
+    print(f"Checking sequences of length {num_items}")
+    if num_items == len(input_items):
+        raise Exception("We didn't find it...")
+    for i, starting_point in enumerate(input_items[: len(input_items) - num_items]):
+        if sum(input_items[i : i + num_items]) == target:
+            return input_items[i : i + num_items]
+    return find_sequence_that_sums(input_items, num_items + 1, target)
+
+
+if __name__ == "__main__":
+    test_input = """35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576"""
+    # res = main(test_input, 127)
+    res = main(input_string, 1398413738)
+    print(f"The result is {res}")
