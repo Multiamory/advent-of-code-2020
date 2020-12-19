2ed806b9ef6430ead1212b630c7418bc50a44632
Jase Lindgren <jase@multiamory.com>
2020-12-19 12:37:54 -0800

    Part 2 required me to clean the whole flow.

--- 2020/18/part_1.py
+++ 2020/18/part_1.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import string
 
 # ___________ Begin Setup and Get Input _____________
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
@@ -11,3 +12,50 @@ input_string = get_input.get_input_for_day(__file__)
 # __________________ End of setup ____________________
 
 
+def main(input_string):
+    lines = input_string.splitlines()
+    total = 0
+    for line in lines:
+        chars = [c for c in line if c != " "]
+        result = calculate_line(chars)
+        total += result
+        print("Result", result)
+    return total
+
+
+def calculate_line(chars, index=None):
+    res = None
+    operator = None
+    if not index:
+        index = {"i": 0}
+    while index["i"] < len(chars):
+        try:
+            char = int(chars[index["i"]])
+        except ValueError:
+            char = chars[index["i"]]
+
+        if char == "(":
+            index["i"] += 1
+            char = calculate_line(chars, index)
+        if char == ")":
+            return res
+
+        if isinstance(char, int):
+            if operator:
+                res = eval(f"{res}{operator}{char}")
+                operator = None
+            else:
+                res = int(char)
+        else:
+            operator = char
+
+        index["i"] += 1
+
+    return res
+
+
+if __name__ == "__main__":
+    test_string = "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"
+    test_string = "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2  "
+    res = main(input_string)
+    print(res)
--- 2020/18/part_2.py
+++ 2020/18/part_2.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import string
 
 # ___________ Begin Setup and Get Input _____________
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
@@ -11,3 +12,56 @@ input_string = get_input.get_input_for_day(__file__)
 # __________________ End of setup ____________________
 
 
+def main(input_string):
+    lines = input_string.splitlines()
+    total = 0
+    for line in lines:
+        line = "".join([c for c in line if c != " "])
+        result = do_line(line)
+        total += result
+        print("Result", result)
+    return total
+
+
+def do_line(line):
+    openings = []
+    pairs = []
+    for i, c in enumerate(line):
+        if c == "(":
+            openings.append(i)
+        elif c == ")":
+            match = openings.pop()
+            if len(openings) == 0:
+                pairs.append((match, i))
+    for pair in reversed(pairs):
+        line = line.replace(
+            line[pair[0] : pair[1] + 1],
+            str(do_line(line[pair[0] + 1 : pair[1]])),
+        )
+
+    return calculate_section(line)
+
+
+def do_addition(section):
+    res = 0
+    numbers = section.split("+")
+    for number in numbers:
+        res += int(number)
+    return res
+
+
+def calculate_section(line):
+    sections = line.split("*")
+    product = 1
+    for section in sections:
+        product *= do_addition(section)
+
+    return product
+
+
+if __name__ == "__main__":
+    # test_string = "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"
+    # test_string = "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"
+    # test_string = "2 * 3 + (4 * 5)"
+    res = main(input_string)
+    print(res)
